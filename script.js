8359e8fe5d9bfa648227698aaff08d7e6c9bebfb
TracyeWilhelm <tracye.wilhelm@gmail.com>
2022-02-10 15:05:08 -0700

    updated js, added h1

--- script.js
+++ script.js
@@ -1,79 +1,122 @@
-//var user = "you"
-//this is the variables that the 
-var rps = ["rock", "paper", "scissors"]
 var win = 0
 var loss = 0
 var ties = 0
+var answers = ["rock", "paper", "scissors",] //this is the list of variables that the user and computer can choose from
+
+ var play 
+setTimeout(function() {
+  alert("Welcome to the game Rock, Paper, Scissors!") 
+   play = window.confirm("Do you want to play?")
+  console.log(play)
+
+  gameplay()
+
+}, 2000);
+//welcome window - alerts the user 'welcome...' - this works
 
 
-//welcome window
-alert("Welcome to the game!")
+
+//ok/cancel window to ask if user wants to play - this works
 
 
-//prompt("Choose Rock, Paper, or Scissors")
-
-var Choice = window.prompt("rock, paper, or scissors?");
-console.log(Choice)
-
-var answers = [
-  "rock",
-  "paper",
-  "scissors",
-]
-
-var randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-
-console.log(randomAnswer);
-
-alert("Computer chose " + randomAnswer)
-
-var outcomes = function () {
   
-  if (Choice == randomAnswer) {
-    console.log("you tied!")
-    alert("You tied!")
-    ties++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
+// if they chose ok (truthy) run code; if they chose cancel window says 'have a nice day' - THIS BAD
+// how do I get it to go back to play function when they close the 'nice day' window?
 
-  } else if (Choice == "rock" && randomAnswer == "scissors") {
-    alert("You win!")
-    win++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
+var gameplay = function () {
+  if (play == false) {
+    console.log(gameplay)
+    window.alert("Have a nice day!")
+  } else {
+    
 
-  } else if (Choice == "rock" && randomAnswer == "paper") {
-    alert("You lose")
-    loss++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
+    Choice = window.prompt("rock, paper, or scissors?"); //prompt asking user to pick r, p, s
+    if (answers.includes(Choice)) {
+         alert("Good choice!")   
+    } else {
+      alert("You can't play Rock, Paper, Scissors with " + Choice + "!") //if the user doesn't pick r, p, s window says 'you can't win with ___'
 
-  } else if (Choice == "paper" && randomAnswer == "rock") {
-    alert("You win!")
-    win++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
+      console.log(Choice) //logs the users choice in the console
+    }
+    var randomAnswer = answers[Math.floor(Math.random() * answers.length)]; //variable that is a function where the computer picks a random answer from the var answer array
+    alert("Computer chooses " + randomAnswer) //window telling the user which answer variable was picked at random
+  
+    var outcomes = function () {
+  
+      if (Choice == randomAnswer) {
+        console.log("you tied!")
+        alert("You tied!")
+        ties++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
 
-  } else if (Choice == "paper" && randomAnswer == "scissors") {
-    alert("You lose")
-    loss++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
+      } else if (Choice == "rock" && randomAnswer == "scissors") {
+        console.log("You win!")
+        alert("You win!")
+        win++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
 
-  } else if (Choice == "scissors" && randomAnswer == "paper") {
-    alert("You win!")
-    win++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
+      } else if (Choice == "rock" && randomAnswer == "paper") {
+        console.log("You lose")
+        alert("You lose")
+        loss++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
 
-  } else if (Choice == "scissors" && randomAnswer == "rock") {
-    alert("You lose")
-    loss++
-    alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
-    return
-  }
+      } else if (Choice == "paper" && randomAnswer == "rock") {
+        console.log("You win!")
+        alert("You win!")
+        win++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
+
+      } else if (Choice == "paper" && randomAnswer == "scissors") {
+        console.log("You lose")
+        alert("You lose")
+        loss++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
+
+      } else if (Choice == "scissors" && randomAnswer == "paper") {
+        console.log("You win!")
+        alert("You win!")
+        win++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
+
+      } else if (Choice == "scissors" && randomAnswer == "rock") {
+        console.log("You lose")
+        alert("You lose")
+        loss++
+        alert("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties)
+        return
+
+      } else if (Choice != answers) {
+        console.log("You lose")
+        alert("You lose")
+      }
+      
+    }
+    console.log(outcomes())
+    var playagain = confirm("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties + "\nPlay again?") 
+    if (playagain == true) {
+      gameplay()
+    } else {
+      alert("Thanks for playing")
+   }
+  } 
+  return
 }
+  
+  // console.log(gameplay())
 
-console.log(outcomes())
 
-confirm("Wins: " + win + "\nLosses: " + loss + "\nTies: " + ties + "\nPlay again?")  
+
+
+
+
+  
+
+ 
+
